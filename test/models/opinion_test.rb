de9f0ff28a4aad45d204b2f8fb07cce17258f738
Thomas Midgett <tmidge20@gmail.com>
2015-02-20 10:35:16 -0500

    adds first tests

--- test/models/opinion_test.rb
+++ test/models/opinion_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class OpinionTest < ActiveSupport::TestCase
+  setup do
+    @opinion = opinions(:one)
+  end
+  
   # test "the truth" do
   #   assert true
   # end
+
+  test "Opinions belong to bills" do
+    assert @opinion.bill
+  end
 end
